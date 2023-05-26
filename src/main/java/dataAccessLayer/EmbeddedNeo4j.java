83dab89cbb6df6524f3ab9a545f3ad9267812dbc
Diego Soto <sot22737@uvg.edu.gt>
2023-05-26 07:09:58 -0600

    1

---
+++ src/main/java/dataAccessLayer/EmbeddedNeo4j.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package dataAccessLayer;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
+import org.neo4j.driver.summary.ResultSummary;
+
+import static org.neo4j.driver.Values.parameters;
+
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * @author Administrator
+ *
+ */
+public class EmbeddedNeo4j implements AutoCloseable{
+
+    private final Driver driver;
+    
+
+    public EmbeddedNeo4j( String uri, String user, String password )
+    {
+        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    }
+
+    @Override
+    public void close() throws Exception
+    {
+        driver.close();
+    }
+
+    
+    
+    public LinkedList<String> getRestaurantsByName(String name)
+    {
+   	 try ( Session session = driver.session() )
+        {
+   		 
+   		 
+   		 LinkedList<String> names = session.readTransaction( new TransactionWork<LinkedList<String>>()
+            {
+                @Override
+                public LinkedList<String> execute( Transaction tx )
+                {
+                    Result result = tx.run( "MATCH (t:Person {name: \"" + name + "\"})-[:LIKES]->(restaurantsPerson) RETURN restaurantsPerson.title");
+                    LinkedList<String> myUsers = new LinkedList<String>();
+                    List<Record> registros = result.list();
+                    for (int i = 0; i < registros.size(); i++) {
+                   	 //myactors.add(registros.get(i).toString());
+                   	 myUsers.add(registros.get(i).get("restaurantsPerson.title").asString());
+                    }
+                    
+                    return myUsers;
+                }
+            } );
+            
+            return names;
+        }
+   }
+
+}
